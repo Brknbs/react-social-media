e0810d32dc2e8823140f377ca03eeac962cd5a8a
Berkan <brknbs@gmail.com>
2020-11-17 13:02:07 +0300

    graphql file organized

--- index.js
+++ index.js
@@ -1,36 +1,11 @@
 const { ApolloServer } = require('apollo-server');
-const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const Post = require('./models/Post');
 const mongodbURI = process.env.MONGODB_URI;
 const port = process.env.PORT || 5000;
-
-const typeDefs = gql`
-  type Post {
-    id: ID!
-    body: String!
-    username: String!
-    createdAt: String!
-  }
-  type Query {
-    getPosts: [Post]
-  }
-`;
-
-const resolvers = {
-  Query: {
-    async getPosts() {
-      try {
-        const posts = await Post.find();
-        return posts;
-      } catch (error) {
-        console.log(error)
-      }
-    }
-  }
-}
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
 
 const server = new ApolloServer({
   typeDefs,
