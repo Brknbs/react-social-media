1fb13f4284758a80e5381eae4e0e761bcb595d88
Berkan Baş <brknbs@gmail.com>
2020-12-05 16:14:54 +0300

    semantic ui added & routes created & navbar created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-function App() {
+import 'semantic-ui-css/semantic.min.css'
+import './App.css';
+import { Container } from 'semantic-ui-react';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+import Navbar from './components/Navbar';
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <Container>
+        <Navbar />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+      </Container>
+    </Router>
   );
 }
 
