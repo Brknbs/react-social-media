ea50abd16a99f40e990c6420dea9360037f25928
Berkan Baş <brknbs@gmail.com>
2020-12-27 18:16:50 +0300

    Created a util to redirect authenticated user to homepage when visited /register or /login

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import { Container } from 'semantic-ui-react';
 
 import { AuthProvider } from './context/auth';
+import AuthRoute from './utils/AuthRoute';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -20,8 +21,8 @@ const App = () => {
         <Container>
           <Navbar />
           <Route exact path='/' component={Home} />
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/register' component={Register} />
+          <AuthRoute exact path='/login' component={Login} />
+          <AuthRoute exact path='/register' component={Register} />
         </Container>
       </Router>
     </AuthProvider>
